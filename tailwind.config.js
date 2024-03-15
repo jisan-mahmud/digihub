fc778584f1033ed3af1c640595c952760e041de2
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-03-15 16:51:46 +0600

    add

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        },
+    },
+  },
+  plugins: [],
+}
